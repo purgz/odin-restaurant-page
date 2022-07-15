fe738ad181fdbb26a36d06dee88a1e9a321693c8
purgz <henbrooks6@gmail.com>
2022-07-15 14:54:38 +0100

    added element to menu page and altered some styles, new build

--- src/modules/home.js
+++ src/modules/home.js
@@ -8,6 +8,19 @@ export function loadHome(){
     //add background image
     const fullPage = document.querySelector("html");
     fullPage.style.backgroundImage = `url(${BackgroundImg})`
+
+    const container = document.createElement("div");
+    const title = document.createElement("h2");
+    const p = document.createElement("p");
+
+    container.classList.add("main-page-container");
+
+    title.textContent = "Welcome to the main page";
+    p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique neque odio, at lacinia erat hendrerit eu. Duis non turpis vehicula, pulvinar dolor ac, varius enim. Vestibulum at dolor in eros dignissim lobortis vitae ac velit. Vestibulum pretium sed neque sed consequat. Nulla vestibulum neque id lacus ullamcorper lobortis."
+
+    container.appendChild(title);
+    container.appendChild(p);
+    content.appendChild(container);
 }
 
 export function loadFooter(){
